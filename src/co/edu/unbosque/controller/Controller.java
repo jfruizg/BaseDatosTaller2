a962f045197948d9cae2b74de6776e93643da678
juanf <jfruizg@unbosque.edu.co>
2021-09-03 11:58:54 -0500

    Version final sin organizar

--- src/co/edu/unbosque/controller/Controller.java
+++ src/co/edu/unbosque/controller/Controller.java
@@ -1,19 +1,17 @@
 package co.edu.unbosque.controller;
 
-import co.edu.unbosque.model.Acces;
+import co.edu.unbosque.model.BaseDatos;
 import co.edu.unbosque.view.View;
-
-import java.util.Locale;
 
 public class Controller {
 
     private View vista;
-    private Acces accesoMDB;
+    private BaseDatos accesoMDB;
     final String ficheroAccess = "Academia.accdb";
 
     public Controller() {
         vista = new View();
-        accesoMDB = new Acces(ficheroAccess);
+        accesoMDB = new BaseDatos(ficheroAccess);
         inicializar();
     }
 
@@ -24,9 +22,8 @@ public class Controller {
         while (CONST == 1) {
             String nombreTabla = "Estudiante";
             if (accesoMDB.conectarBD()) {
-                accesoMDB.mostrarRegistrosTabla1("Estudiante", "Apellidos", "Nombres", "Sexo", "Edad", "Curso");
                 vista.mostrarDatos("Número registros tabla Contactos: " + accesoMDB.numRegistros("Estudiante"));
-                vista.mostrarDatos("Registrar -> [1]" + "\n" + "Consultar por curso -> [2]"+"\n"+ "Terminar -> [3]");
+                vista.mostrarDatos("Registrar -> [1]" + "\n" + "Consultar por curso -> [2]" + "\n" + "Terminar -> [3]");
                 int menu = Integer.parseInt(vista.recibirDatos());
                 if (menu == 1) {
                     vista.mostrarDatos("Ingrese Apellido");
@@ -41,19 +38,25 @@ public class Controller {
                     vista.mostrarDatos("Ingrese curso");
                     String curso = vista.recibirDatos();
 
-                    if(sexo.length() == 1){
-                        accesoMDB.registrarEstudiante(nombreTabla, apellido, nombre, sexoM, edad, curso);
-                    }else {
-                        vista.mostrarDatos("Escribir en genero solo"+"\n"+"Masuclino = M"+"\n"+"Femenino = F"+"\n");
+                    try {
+
+                        if (sexo.length() == 1) {
+                            accesoMDB.registrarEstudiante(nombreTabla, apellido, nombre, sexoM, edad, curso);
+                        } else {
+                            vista.mostrarDatos("Escribir en genero solo" + "\n" + "Masuclino = M" + "\n" + "Femenino = F" + "\n");
+                        }
+                    }catch (NumberFormatException e){
+                        vista.mostrarDatos("Inrgrese numeros en lso campos debidos");
                     }
                 }
                 if (menu == 2) {
-
                     vista.mostrarDatos("Escribe el curso que deseas buscar");
-                    String curso= vista.recibirDatos();
-
-                    accesoMDB.buscarEstudiante(nombreTabla,"Curso",curso,"Apellidos", "Nombres", "Sexo", "Edad", "Curso");
-
+                    String curso = vista.recibirDatos();
+                    if (!curso.equals("")) {
+                        vista.mostrarDatos(accesoMDB.buscarRegistroValor(nombreTabla, "Curso", "Access", "Apellidos", "Nombres", "Sexo", "Edad", "Curso"));
+                    } else {
+                        accesoMDB.mostrarRegistrosTabla1("Estudiante", "Apellidos", "Nombres", "Sexo", "Edad", "Curso");
+                    }
                 }
                 if (menu == 3) {
                     accesoMDB.desconectarBD();
---
+++ src/co/edu/unbosque/model/BaseDatos.java
@@ -0,0 +1,115 @@
+package co.edu.unbosque.model;
+
+import com.healthmarketscience.jackcess.Cursor;
+import com.healthmarketscience.jackcess.CursorBuilder;
+import com.healthmarketscience.jackcess.Database;
+import com.healthmarketscience.jackcess.DatabaseBuilder;
+import com.healthmarketscience.jackcess.Row;
+import com.healthmarketscience.jackcess.Table;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class BaseDatos {
+    private String rutaBD;
+    private Database BDAcces;
+
+    public BaseDatos(String rutaBD) {
+        this.rutaBD = rutaBD;
+    }
+
+    public boolean conectarBD() {
+        try {
+            BDAcces = DatabaseBuilder.open(new File(rutaBD));
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public void mostrarRegistrosTabla1(String nombreTabla, String campo1, String campo2, String campo3, String campo4,
+                                       String campo5) {
+        try {
+            Table tabla = BDAcces.getTable(nombreTabla);
+            for (Row registro : tabla) {
+                System.out.println("-------------------------------------");
+                if (!campo1.equalsIgnoreCase(""))
+                    System.out.println(campo1 + ": " + registro.get(campo1));
+                if (!campo2.equalsIgnoreCase(""))
+                    System.out.println(campo2 + ": " + registro.get(campo2));
+                if (!campo3.equalsIgnoreCase(""))
+                    System.out.println(campo3 + ": " + registro.get(campo3));
+                if (!campo4.equalsIgnoreCase(""))
+                    System.out.println(campo4 + ": " + registro.get(campo4));
+                if (!campo5.equalsIgnoreCase(""))
+                    System.out.println(campo5 + ": " + registro.get(campo5));
+
+            }
+        } catch (Exception e) {
+            System.out.println("Error al mostrar tabla [" + nombreTabla + "]: " + e.getMessage());
+        }
+    }
+    public void registrarEstudiante(String nombreTabla, String apellido, String nombre, String sxo, int edad, String curso) {
+        try {
+            Table table = BDAcces.getTable(nombreTabla);
+            table.addRow(apellido, nombre, sxo, edad, curso);
+        } catch (Exception e) {
+            System.out.println("Error al insertar registro en tabla [" + nombreTabla + "]: " + e.getMessage());
+        }
+    }
+    public String buscarRegistroValor(String nombreTabla, String campo,
+                                      String filtro, String campo1, String campo2, String campo3, String campo4,
+                                      String campo5)
+    {
+        String valor = "";
+        try
+        {
+            Table tabla = BDAcces.getTable(nombreTabla);
+            Cursor cursor = CursorBuilder.createCursor(tabla);
+            for (Row registro : cursor.newIterable().addMatchPattern(campo, filtro))
+            {
+                System.out.println("-------------------------------------");
+                if (!campo1.equalsIgnoreCase(""))
+                    System.out.println(campo1 + ": " + registro.get(campo1));
+                if (!campo2.equalsIgnoreCase(""))
+                    System.out.println(campo2 + ": " + registro.get(campo2));
+                if (!campo3.equalsIgnoreCase(""))
+                    System.out.println(campo3 + ": " + registro.get(campo3));
+                if (!campo4.equalsIgnoreCase(""))
+                    System.out.println(campo4 + ": " + registro.get(campo4));
+                if (!campo5.equalsIgnoreCase(""))
+                    System.out.println(campo5 + ": " + registro.get(campo5));
+            }
+            return valor;
+        }
+        catch (Exception e)
+        {
+            System.out.println ("Error obtener valor de tabla [" + nombreTabla + "]: " +
+                    e.getMessage());
+            return  "";
+        }
+    }
+
+    public int numRegistros(String nombreTabla) {
+        try {
+            Table tabla = BDAcces.getTable(nombreTabla);
+            return tabla.getRowCount();
+        } catch (Exception e) {
+            System.out.println("Error al contar registros de [" + nombreTabla + "]: " + e.getMessage());
+            return 0;
+
+        }
+    }
+
+    public void desconectarBD() {
+        try {
+            BDAcces.close();
+        } catch (Exception e) {
+            System.out.println("Error al cerrar BD [" + rutaBD + "]: " + e.getMessage());
+        }
+    }
+}
+
